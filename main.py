919785ed9d81a1194d973ee9dac1e2968b780f8e
R-lab <golledgekb@yahoo.com>
2025-11-15 10:47:22 -0800

    B: created stats.py and moved the count def to stats.py

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
+from stats import count_words
+
 
 book = "/home/boot/bookbot/books/frankenstein.txt"
 
+    
 def get_book_text():
     with open(book) as f:
         return f.read()
-    
+
 def main():
     text = get_book_text()
-    print(text)
-    
-main()
+    print(f"Found {count_words(text)} total words")
+       
+if __name__ == "__main__":
+    main()
---
+++ stats.py
@@ -0,0 +1,8 @@
+def count_words(text):
+    words = text.split()
+    return len(words)
+
+
+
+    
+    
